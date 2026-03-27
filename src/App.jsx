28d8707d83274006aa747a126cd44a0a397d8c98
Emanuel Messias Vieira De Araújo <emanuelmvieira10@gmail.com>
2026-03-27 16:15:59 -0300

    feat: adicionando integracao com api rawg no app

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,39 @@
-import { useState } from 'react'
-
+import { useState, useEffect } from 'react'
+import Navbar from './components/Navbar'
+import CardGame from './components/CardGame'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [games, setGames] = useState([])
+  const API_KEY = 'bf218c5015344fb99622a8ad0581161ea'
+
+  // 1. Função de busca usando .then()
+  const fetchGames = (search = '') => {
+    const url = `https://api.rawg.io/api/games?key=${API_KEY}&search=${search}&page_size=12`
+    
+    fetch(url)
+      .then(response => response.json()) 
+      .then(data => setGames(data.results))
+  }
+
+  
+  useEffect(() => {
+    fetchGames()
+  }, [])
 
   return (
-    <h1 className="text-5xl font-bold text-red-500 bg-yellow-400">
-  TESTE TAILWIND </h1>
-   
+    <div className="bg-slate-950 min-h-screen text-white p-5">
+      {}
+      <Navbar onSearch={fetchGames} />
+
+      <main className="mt-10">
+        <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6">
+          {}
+          {games.map(item => (
+            <CardGame key={item.id} game={item} />
+          ))}
+        </div>
+      </main>
+    </div>
   )
 }
 
