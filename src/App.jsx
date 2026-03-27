30ee991197130c7a3be7011d5053ec479da1e7f0
Emanuel Messias Vieira De Araújo <emanuelmvieira10@gmail.com>
2026-03-27 15:49:57 -0300

    Primeiraversão

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <h1 className="text-5xl font-bold text-red-500 bg-yellow-400">
+  TESTE TAILWIND </h1>
+   
+  )
+}
+
+export default App
